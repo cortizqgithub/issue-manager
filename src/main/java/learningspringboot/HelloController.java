1de8fe6f1ac151f706e3e1cb44a6f5600d68113d
Carlos Adolfo Ortiz Quiros (Mac) <carlosaortiz@yahoo.com>
2016-02-28 13:00:19 -0500

    Adds Devtools and fixes Java Imports

--- src/main/java/learningspringboot/HelloController.java
+++ src/main/java/learningspringboot/HelloController.java
@@ -1,12 +1,10 @@
 package learningspringboot;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HelloController {
 	
-	@RequestMapping(value = "/hello/")
+	@RequestMapping(value = "/hello")
 	public String index() {		
 		return "hello";
 	}
--- src/main/java/learningspringboot/IssueController.java
+++ src/main/java/learningspringboot/IssueController.java
@@ -1,6 +1,4 @@
 package learningspringboot;
-import java.util.ArrayList;
-import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
